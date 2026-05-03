e6f270c81aeef2d4deb35321d918bd9880b6bbed
trendz113 <79502794+trendz113@users.noreply.github.com>
2026-05-03 08:05:31 +0530

    Update create-order.js

--- api/create-order.js
+++ api/create-order.js
@@ -7,7 +7,7 @@ const rzp = new Razorpay({
 });
 
 module.exports = async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://salarybit.in');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 

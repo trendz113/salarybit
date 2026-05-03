b9c7b1938e357f1114dbb7c1925329fe0c61a377
trendz113 <79502794+trendz113@users.noreply.github.com>
2026-05-03 08:04:54 +0530

    Update verify-and-decode.js

--- api/verify-and-decode.js
+++ api/verify-and-decode.js
@@ -15,7 +15,7 @@ function ensureDeps() {
 }
 
 module.exports = async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://salarybit.in');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
